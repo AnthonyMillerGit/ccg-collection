da2c11ea596a7dea3bdb71d1c2f7b716067f6e32
Anthony Miller <anthony@amiller.info>
2024-06-06 15:04:52 -0700

    Initial setup with React front-end

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Card Collection</h1>
+    </div>
+  );
+}
+
+export default App;
